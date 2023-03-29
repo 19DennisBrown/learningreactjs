35df452d8aae9403cbab6dc68bfe359bff059b42
19DennisBrown <107542527+19DennisBrown@users.noreply.github.com>
2023-03-29 12:51:45 +0300

    additional codes

--- src/Aside.js
+++ src/Aside.js
@@ -1,21 +1,63 @@
 
 
 import React from 'react'
+import Cityone from './images/cityone.jpeg'
+import citytwo from './images/citytwo.jpeg'
+import citythree from './images/citythree.jpeg'
+import cityfour from './images/cityone.jpeg'
 
+const Images=()=>{
+  return(
+    <section>
+      <section className="img--collection">
+        <div>
+          <img src={Cityone} alt="" />  
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam soluta dolorum optio!
+            <b>Ohio, Japan</b>
+          </p>
+        </div>
+        <div>
+          <img src={citytwo} alt="" />  
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam soluta dolorum optio!
+            <b>Ohio, Japan</b>
+          </p>
+        </div>
+        <div>
+          <img src={citythree} alt="" />  
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam soluta dolorum optio!
+            <b>Ohio, Japan</b>
+          </p>
+        </div>
+        <div>
+          <img src={cityfour} alt="" />  
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam soluta dolorum optio!
+            <b>Ohio, Japan</b>
+          </p>
+        </div>
+         
+      </section>
+    </section>
+  )
+}
 
 const Aside = () => {
   return (
     <div>
       <>
-      <h1>
-        Aside.
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nobis aspernatur ea nulla. Nemo ut est doloribus adipisci excepturi voluptate fugit officia nulla, alias quos nihil rerum harum, ab dolore earum natus maiores ratione delectus asperiores libero? Dolorem, eaque nostrum?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta provident nesciunt odit natus eligendi fugit quis cum, corrupti earum unde enim ipsam quod, aperiam nisi delectus reprehenderit eum animi dolores sit iure. Vero perspiciatis optio blanditiis consequatur possimus minus eveniet ullam nulla nihil consectetur quisquam ad, perferendis hic nam dolore soluta. Sint, quibusdam magni. Odit ullam voluptatum incidunt vel rem, at architecto reprehenderit expedita veritatis id, officiis culpa assumenda neque, laboriosam minima cumque animi fuga nesciunt explicabo adipisci vero! Quia illum asperiores et qui cumque perferendis tempore illo repudiandae accusantium unde earum nihil autem obcaecati perspiciatis cum dolore voluptas, alias nemo ex quam voluptate. Maiores ad in perferendis quasi, rem alias quia ratione iste blanditiis natus cum amet fugit impedit necessitatibus possimus ipsam sit corporis, repellendus accusantium architecto beatae quos ut! Ex dicta alias quasi ipsum natus illum minus quam rem! Necessitatibus soluta fuga incidunt ex, culpa iusto nemo nobis ad facere! Est ullam nostrum laudantium reiciendis suscipit maxime dolores alias sed officiis excepturi! Tempore eius, distinctio modi omnis odio provident necessitatibus rem illo doloribus illum reprehenderit in accusamus nobis officia! Officia iure accusamus vel, quibusdam aperiam doloribus esse. Facere accusantium magni libero assumenda eum, iste quaerat at error sapiente!
-      </p>
+        <Images/>
+        <h1>
+          Aside.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nobis aspernatur ea nulla. Nemo ut est doloribus adipisci excepturi voluptate fugit officia nulla, alias quos nihil rerum harum, ab dolore earum natus maiores ratione delectus asperiores libero? Dolorem, eaque nostrum?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta provident nesciunt odit natus eligendi fugit quis cum, corrupti earum unde enim ipsam quod, aperiam nisi delectus reprehenderit eum animi dolores sit iure. Vero perspiciatis optio blanditiis consequatur possimus minus eveniet ullam nulla nihil consectetur quisquam ad, perferendis hic nam dolore soluta. Sint, quibusdam magni. Odit ullam voluptatum incidunt vel rem, at architecto reprehenderit expedita veritatis id, officiis culpa assumenda neque, laboriosam minima cumque animi fuga nesciunt explicabo adipisci vero! Quia illum asperiores et qui cumque perferendis tempore illo repudiandae accusantium unde earum nihil autem obcaecati perspiciatis cum dolore voluptas, alias nemo ex quam voluptate. Maiores ad in perferendis quasi, rem alias quia ratione iste blanditiis natus cum amet fugit impedit necessitatibus possimus ipsam sit corporis, repellendus accusantium architecto beatae quos ut! Ex dicta alias quasi ipsum natus illum minus quam rem! Necessitatibus soluta fuga incidunt ex, culpa iusto nemo nobis ad facere! Est ullam nostrum laudantium reiciendis suscipit maxime dolores alias sed officiis excepturi! Tempore eius, distinctio modi omnis odio provident necessitatibus rem illo doloribus illum reprehenderit in accusamus nobis officia! Officia iure accusamus vel, quibusdam aperiam doloribus esse. Facere accusantium magni libero assumenda eum, iste quaerat at error sapiente!
+        </p>
 
-      </>
+        </>
     </div>
 
   )
